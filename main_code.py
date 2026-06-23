2d0370c86240645a4d39597d699db3d442c985c0
kashvibajpai1 <bajpaikashvi70@gmail.com>
2026-06-23 21:15:51 +0530

    Remove old main_code.py (renamed to Band-Name Generator.py)

--- main_code.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to Band-Name Generator")
-
-city=input("Whats the name of the city you grew up in?\nEnter:")
-pet=input("Whats your pet name?\nEnter:")
-
-print("The name of your band could be" + " "+ city + " " + pet)
-
