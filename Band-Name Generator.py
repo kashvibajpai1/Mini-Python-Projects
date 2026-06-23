36fa5d5ecb222b0dc9d6bfb1f595c02c06af65a5
kashvibajpai1 <bajpaikashvi70@gmail.com>
2026-06-23 21:15:50 +0530

    Rename main_code.py to Band-Name Generator.py

---
+++ Band-Name Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Band-Name Generator")
+
+city=input("Whats the name of the city you grew up in?\nEnter:")
+pet=input("Whats your pet name?\nEnter:")
+
+print("The name of your band could be" + " "+ city + " " + pet)
+
